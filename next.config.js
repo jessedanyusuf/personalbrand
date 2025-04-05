28c5b74e71e10d211fd45c3935aaa60267fd47b2
Jesse Dan-Yusuf <jessedanyusuf@gmail.com>
2025-04-05 02:59:19 +0100

    Configure Next.js for static export

--- next.config.js
+++ next.config.js
@@ -1,15 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'files.selar.co',
-      },
-    ],
-  },
-  // Set strict mode to ensure consistent rendering
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig;
+module.exports = { output: "export", images: { unoptimized: true } };
